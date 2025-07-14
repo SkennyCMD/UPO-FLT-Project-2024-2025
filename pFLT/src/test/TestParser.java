0a42df601c0b08307ef4844cfb7331ceea4fa527
Matteo Schintu <matteo170803@gmail.com>
2025-07-14 02:36:13 +0200

    Parser's tests minor changes

--- pFLT/src/test/TestParser.java
+++ pFLT/src/test/TestParser.java
@@ -13,87 +13,77 @@ import scanner.*;
 public class TestParser {
     @Test
     void testParserCorretto1() throws FileNotFoundException, SyntacticException {
-        Scanner scanner = new Scanner("src/test/TestFiles/parser/testParserCorretto1.txt");
-        Parser parser = new Parser(scanner);
+        Parser parser = new Parser(new Scanner("src/test/TestFiles/parser/testParserCorretto1.txt"));
 
         Assertions.assertDoesNotThrow(() -> parser.parse());
     }
 
     @Test
     void testParserCorretto2() throws FileNotFoundException, SyntacticException {
-        Scanner scanner = new Scanner("src/test/TestFiles/parser/testParserCorretto2.txt");
-        Parser parser = new Parser(scanner);
+        Parser parser = new Parser(new Scanner("src/test/TestFiles/parser/testParserCorretto2.txt"));
 
         Assertions.assertDoesNotThrow(() -> parser.parse());
     }
     
     @Test
-    void testParserEcc0() throws FileNotFoundException, SyntacticException {
-        Scanner scanner = new Scanner("src/test/TestFiles/parser/testParserEcc_0.txt");
-        Parser parser = new Parser(scanner);
+    void testParserEcc_0() throws FileNotFoundException, SyntacticException {
+        Parser parser = new Parser(new Scanner("src/test/TestFiles/parser/testParserEcc_0.txt"));
 
         Exception e = Assertions.assertThrows(SyntacticException.class, () -> parser.parse());
         assertEquals("ASSIGN or OP_ASSIGN expected, obtained: SEMI", e.getMessage());
     }
 
     @Test
-    void testParserEcc1() throws FileNotFoundException, SyntacticException {
-        Scanner scanner = new Scanner("src/test/TestFiles/parser/testParserEcc_1.txt");
-        Parser parser = new Parser(scanner);
+    void testParserEcc_1() throws FileNotFoundException, SyntacticException {
+        Parser parser = new Parser( new Scanner("src/test/TestFiles/parser/testParserEcc_1.txt"));
 
         Exception e = Assertions.assertThrows(SyntacticException.class, () -> parser.parse());
         assertEquals("ID, INT or FLOAT expected, obtained: TIMES", e.getMessage());
     }
 
     @Test
-    void testParserEcc2() throws FileNotFoundException, SyntacticException {
-        Scanner scanner = new Scanner("src/test/TestFiles/parser/testParserEcc_2.txt");
-        Parser parser = new Parser(scanner);
+    void testParserEcc_2() throws FileNotFoundException, SyntacticException {
+        Parser parser = new Parser(new Scanner("src/test/TestFiles/parser/testParserEcc_2.txt"));
 
         Exception e = Assertions.assertThrows(SyntacticException.class, () -> parser.parse());
         assertEquals("TYINT, TYFLOAT, PRINT, ID or EOF expected, obtained: INT", e.getMessage());
     }
 
     @Test
-    void testParserEcc3() throws FileNotFoundException, SyntacticException {
-        Scanner scanner = new Scanner("src/test/TestFiles/parser/testParserEcc_3.txt");
-        Parser parser = new Parser(scanner);
+    void testParserEcc_3() throws FileNotFoundException, SyntacticException {
+        Parser parser = new Parser(new Scanner("src/test/TestFiles/parser/testParserEcc_3.txt"));
 
         Exception e = Assertions.assertThrows(SyntacticException.class, () -> parser.parse());
         assertEquals("ASSIGN or OP_ASSIGN expected, obtained: PLUS", e.getMessage());
     }
 
     @Test
-    void testParserEcc4() throws FileNotFoundException, SyntacticException {
-        Scanner scanner = new Scanner("src/test/TestFiles/parser/testParserEcc_4.txt");
-        Parser parser = new Parser(scanner);
+    void testParserEcc_4() throws FileNotFoundException, SyntacticException {
+        Parser parser = new Parser(new Scanner("src/test/TestFiles/parser/testParserEcc_4.txt"));
 
         Exception e = Assertions.assertThrows(SyntacticException.class, () -> parser.parse());
         assertEquals("Syntax Error at row 2: Expected: ID, Actual: INT", e.getMessage());
     }
 
     @Test
-    void testParserEcc5() throws FileNotFoundException, SyntacticException {
-        Scanner scanner = new Scanner("src/test/TestFiles/parser/testParserEcc_5.txt");
-        Parser parser = new Parser(scanner);
+    void testParserEcc_5() throws FileNotFoundException, SyntacticException {
+        Parser parser = new Parser(new Scanner("src/test/TestFiles/parser/testParserEcc_5.txt"));
 
         Exception e = Assertions.assertThrows(SyntacticException.class, () -> parser.parse());
         assertEquals("Syntax Error at row 3: Expected: ID, Actual: INT", e.getMessage());
     }
 
     @Test
-    void testParserEcc6() throws FileNotFoundException, SyntacticException {
-        Scanner scanner = new Scanner("src/test/TestFiles/parser/testParserEcc_6.txt");
-        Parser parser = new Parser(scanner);
+    void testParserEcc_6() throws FileNotFoundException, SyntacticException {
+        Parser parser = new Parser(new Scanner("src/test/TestFiles/parser/testParserEcc_6.txt"));
 
         Exception e = Assertions.assertThrows(SyntacticException.class, () -> parser.parse());
         assertEquals("Syntax Error at row 3: Expected: ID, Actual: TYFLOAT", e.getMessage());
     }
 
     @Test
-    void testParserEcc7() throws FileNotFoundException, SyntacticException {
-        Scanner scanner = new Scanner("src/test/TestFiles/parser/testParserEcc_7.txt");
-        Parser parser = new Parser(scanner);
+    void testParserEcc_7() throws FileNotFoundException, SyntacticException {
+        Parser parser = new Parser(new Scanner("src/test/TestFiles/parser/testParserEcc_7.txt"));
 
         Exception e = Assertions.assertThrows(SyntacticException.class, () -> parser.parse());
         assertEquals("Syntax Error at row 2: Expected: ID, Actual: ASSIGN", e.getMessage());
@@ -101,17 +91,15 @@ public class TestParser {
     
     @Test
     void testSoloDich() throws FileNotFoundException, SyntacticException {
-        Scanner scanner = new Scanner("src/test/TestFiles/parser/testSoloDich.txt");
-        Parser parser = new Parser(scanner);
-
+        Parser parser = new Parser(new Scanner("src/test/TestFiles/parser/testSoloDich.txt"));
+        
         Assertions.assertDoesNotThrow(() -> parser.parse());
     }
     
     @Test
     void testSoloDichPrint() throws FileNotFoundException, SyntacticException {
-        Scanner scanner = new Scanner("src/test/TestFiles/parser/testSoloDichPrint.txt");
-        Parser parser = new Parser(scanner);
-
+        Parser parser = new Parser(new Scanner("src/test/TestFiles/parser/testSoloDichPrint.txt"));
+        
         Assertions.assertDoesNotThrow(() -> parser.parse());
     }
 }
